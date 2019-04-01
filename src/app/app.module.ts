7cfc213be28e54517c75553c4a29eb9ff78419d7
jamesandere <jamesandere56@gmail.com>
2019-04-01 09:03:51 +0300

    Add imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { DetailspageService} from './geet/detailspage.service';
+
 
 import { AppComponent } from './app.component';
+import { DetailspageComponent } from './detailspage/detailspage.component';
+import { InformationComponent } from './information/information.component';
+import { ReposComponent } from './repos/repos.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailspageComponent,
+    InformationComponent,
+    ReposComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DetailspageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
