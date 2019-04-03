07e7cd867c72980d2e1b7f09db09a985591ff9a3
jamesandere <jamesandere56@gmail.com>
2019-04-03 10:21:13 +0300

    Create another pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,30 @@ import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { DetailspageService} from './geet/detailspage.service';
 
-
 import { AppComponent } from './app.component';
 import { DetailspageComponent } from './detailspage/detailspage.component';
+import { LandingComponent } from './landing/landing.component';
+import { RouterModule, Routes } from '@angular/router';
+import { DateCountPipe } from './date-count.pipe';
 
+const routes:Routes=[
+  {path:"detailspage",component:DetailspageComponent},
+  
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    DetailspageComponent
+    DetailspageComponent,
+    LandingComponent,
+    DateCountPipe
 
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [DetailspageService],
   bootstrap: [AppComponent]
