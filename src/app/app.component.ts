2d1ee3e69c86cba23242eefd633de4552897f9be
jamesandere <jamesandere56@gmail.com>
2019-04-03 10:11:37 +0300

    Import jQuery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Githubsearch';
+  public ngOnInit(){
+    $(document).ready(function(){
+     $('#but').click(function(){
+       $('#land').hide();
+       $('.container-fluid').show();
+     })
+    });
+  }
 }
